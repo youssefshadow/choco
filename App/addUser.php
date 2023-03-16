fa0113b64c16a18854d1323ed377302705cce187
youssefshadow <youssef20ben@gmail.com>
2023-03-16 15:55:36 +0100

    project_start

---
+++ App/addUser.php
@@ -0,0 +1,3 @@
+<?php
+    //logique PHP
+?>
---
+++ App/vue/view_add_user.php
@@ -0,0 +1,14 @@
+<!-- partie affichage HTML -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Public/asset/style/main.css">
+    <script src="../../Public/asset/script/script.js" defer></script>
+    <title>Inscription</title>
+</head>
+<body>
+    
+</body>
+</html>
