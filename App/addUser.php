902c36c29fe7aca437cfbccc86937380e90c158f
Youssef Benslimane <98457580+youssefshadow@users.noreply.github.com>
2023-03-30 21:13:13 +0200

    Delete App directory

--- App/addUser.php
+++
@@ -1,3 +0,0 @@
-<?php
-    //logique PHP
-?>
--- App/utils/connectBdd.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $bdd = new PDO('mysql:host=localhost;dbname=chocoblast', 'root','', 
-    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-?>
